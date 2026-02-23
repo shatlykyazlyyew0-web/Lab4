fcd4ccd76e2598871d4d529540431edea5d74602
shatlykyazlyyew0-web <shatlykyazlyyew0@gmail.com>
2026-02-23 19:01:04 +0700

    Add files via upload

---
+++ lab4.2.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main() {
+	char str_1[] = "12345";
+	int result = 0;
+	for (int i = 0; str_1[i] != '\0'; i++) {
+		if ((str_1[i] < '0') || (str_1[i] > '9'))
+		{
+			printf("error!!!");
+			return 1;
+		}
+		result = result * 10 + (str_1[i] - '0');
+	}
+	printf("%d", result);
+	return 0;
+}
